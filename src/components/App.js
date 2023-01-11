3d2ed4093c22447ebc32d42aebcc65a9ceda21c0
sangitasarkar74 <sangitasarkar@gmail.com>
2023-01-11 17:26:36 +0530

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
+		this.state = {
+			showText: false,
+		};
+		this.handleClick = this.handleClick.bind(this);
 	};
+	handleClick() {
+		this.setState(state => ({
+			showText: !state.showText
+		}));
+	}
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				{ /* Do not remove this main div!! */}
+				<button id="click" onClick={this.handleClick}>Click here</button>
+				{this.state.showText && <p id="para">Hello, I've learnt to
+					use the full-stack evaluation tool. This makes me so happy</p>}
+			</div>
+		);
+	}
 }
 
 
